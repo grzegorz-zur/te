2db6c33ee93aabab02fdbfb86201c9e449cf8149
Grzegorz Żur <grzegorz.zur@gmail.com>
2022-05-31 19:59:50 +0200

    Listing files

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,13 @@
+use std::env::current_dir;
 use std::error::Error;
 use std::io::{stdin, stdout, Stdout, Write};
+use std::path::PathBuf;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 use termion::screen::AlternateScreen;
-use termion::{clear, cursor, terminal_size};
+use termion::{clear, color, cursor, terminal_size};
+use walkdir::WalkDir;
 
 enum Mode {
     Command,
@@ -12,13 +15,23 @@ enum Mode {
 }
 
 pub struct Editor {
+    files: Files,
     mode: Mode,
     run: bool,
+}
+
+struct Files {
+    path: PathBuf,
+    list: Vec<PathBuf>,
 }
 
 impl Editor {
     pub fn create() -> Editor {
         Editor {
+            files: Files {
+                path: PathBuf::new(),
+                list: vec![],
+            },
             mode: Mode::Command,
             run: true,
         }
@@ -28,9 +41,8 @@ impl Editor {
         let mut term = AlternateScreen::from(stdout().into_raw_mode()?);
         let mut keys = stdin().keys();
         while self.run {
-            write!(term, "{}", clear::All)?;
-            let (columns, rows) = terminal_size()?;
-            self.render(&mut term, columns, rows)?;
+            self.clear(&mut term)?;
+            self.render(&mut term)?;
             term.flush()?;
             if let Some(Ok(key)) = keys.next() {
                 self.handle(key)?;
@@ -39,35 +51,47 @@ impl Editor {
         Ok(())
     }
 
-    fn render(
-        &self,
-        term: &mut RawTerminal<Stdout>,
-        columns: u16,
-        rows: u16,
-    ) -> Result<(), Box<dyn Error>> {
+    fn render(&self, term: &mut RawTerminal<Stdout>) -> Result<(), Box<dyn Error>> {
         match self.mode {
-            Mode::Command => self.render_command(term, columns, rows),
-            Mode::Switch => self.render_switch(term, columns, rows),
+            Mode::Command => self.render_command(term),
+            Mode::Switch => self.render_switch(term),
         }
     }
 
-    fn render_command(
-        &self,
-        term: &mut RawTerminal<Stdout>,
-        _columns: u16,
-        _rows: u16,
-    ) -> Result<(), Box<dyn Error>> {
-        write!(term, "{}command", cursor::Goto(1, 1))?;
+    fn render_command(&self, _term: &mut RawTerminal<Stdout>) -> Result<(), Box<dyn Error>> {
         Ok(())
     }
 
-    fn render_switch(
-        &self,
-        term: &mut RawTerminal<Stdout>,
-        _columns: u16,
-        _rows: u16,
-    ) -> Result<(), Box<dyn Error>> {
-        write!(term, "{}switch", cursor::Goto(1, 1))?;
+    fn render_switch(&self, term: &mut RawTerminal<Stdout>) -> Result<(), Box<dyn Error>> {
+        let (_columns, rows) = terminal_size()?;
+        let mut row = 1;
+        for file in &self.files.list {
+            write!(term, "{}{}", cursor::Goto(1, row), file.display())?;
+            row += 1;
+            if row == rows {
+                break;
+            }
+        }
+        write!(
+            term,
+            "{}{}{}{}",
+            cursor::Goto(1, rows),
+            color::Bg(color::Blue),
+            self.files.path.display(),
+            clear::AfterCursor
+        )?;
+        Ok(())
+    }
+
+    fn clear(&self, term: &mut RawTerminal<Stdout>) -> Result<(), Box<dyn Error>> {
+        write!(
+            term,
+            "{}{}{}{}",
+            color::Fg(color::Reset),
+            color::Bg(color::Reset),
+            clear::All,
+            cursor::Goto(1, 1)
+        )?;
         Ok(())
     }
 
@@ -79,7 +103,10 @@ impl Editor {
     }
     fn handle_command(&mut self, key: Key) -> Result<(), Box<dyn Error>> {
         match key {
-            Key::Char('\t') => self.mode = Mode::Switch,
+            Key::Char('\t') => {
+                self.mode = Mode::Switch;
+                self.list_files()?;
+            }
             Key::Char('B') => self.run = false,
             _ => {}
         }
@@ -90,6 +117,23 @@ impl Editor {
         match key {
             Key::Char('\t') => self.mode = Mode::Command,
             _ => {}
+        }
+        Ok(())
+    }
+
+    fn list_files(&mut self) -> Result<(), Box<dyn Error>> {
+        self.files.path = current_dir()?;
+        self.files.list.clear();
+        for file in WalkDir::new(self.files.path.as_path())
+            .follow_links(true)
+            .sort_by_file_name()
+            .into_iter()
+            .filter_map(|file| file.ok())
+        {
+            if file.metadata()?.is_file() {
+                let relative = file.path().strip_prefix(self.files.path.as_path())?;
+                self.files.list.push(relative.to_path_buf());
+            }
         }
         Ok(())
     }
